5c5abae769dc7fbef080fe0b481e763e2f1552c9
清遠翔太 <shota@shinenshoutanoMacBook-Air-2.local>
2023-11-29 20:36:43 +0900

    feat: 不正な値を入力した際のバリデーションを追加

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import sys
 import random
 
-number1 = int(input('give me first number: '))
-number2 = int(input('give me second number: '))
+while True:
+    try:
+        number1 = int(input('give me first number: '))
+        break
+    except ValueError:
+        print("It's not number")
 
-number3 = random.randint(number1, number2)
+while True:
+    try:
+        number2 = int(input('give me second number: '))
+        break
+    except ValueError:
+        print("It's not number")
+
+
+if number1 <= number2:
+    number3 = random.randint(number1, number2)
+else:
+    number3 = random.randint(number2, number1)
+
 
 while True:
     user_input = int(input('guess the number: '))
